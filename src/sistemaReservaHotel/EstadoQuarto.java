3e26f1f1102e8d86dccca2fbd575b21662fc6059
Bianca Letícia <biancaleticia.moura@gmail.com>
2024-04-30 20:47:29 -0300

    Criando classe Quarto e enum EstadoQuarto

---
+++ src/sistemaReservaHotel/EstadoQuarto.java
@@ -0,0 +1,5 @@
+package sistemaReservaHotel;
+
+public enum EstadoQuarto {
+	ocupado, limpo, sujo
+}
---
+++ src/sistemaReservaHotel/Quarto.java
@@ -0,0 +1,8 @@
+package sistemaReservaHotel;
+
+public class Quarto {
+	int numero;
+	EstadoQuarto estado = EstadoQuarto.limpo;
+	int capacidade = 4; 
+	
+}
