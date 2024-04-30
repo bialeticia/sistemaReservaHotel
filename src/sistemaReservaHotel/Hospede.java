3505377263df31afccc1e3d0e63bf95da7aea72a
Bianca Letícia <biancaleticia.moura@gmail.com>
2024-04-30 20:52:25 -0300

    Atualizando Hospede

--- src/sistemaReservaHotel/Hospede.java
+++ src/sistemaReservaHotel/Hospede.java
@@ -1,5 +1,14 @@
 package sistemaReservaHotel;
 
 public class Hospede {
+	private String nome;
+	private Quarto quarto;
+	
+	public Hospede(String nome) {
+		this.nome = nome;
+	}
 
+	public void setQuarto(Quarto quarto) {
+		this.quarto = quarto;
+	}
 }
