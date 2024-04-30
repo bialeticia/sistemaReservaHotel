b0492f6d00fb4c4cff869382be6595dafd88a8db
Bianca Letícia <biancaleticia.moura@gmail.com>
2024-04-30 20:53:43 -0300

    Adicionando Thread ao Hospede

--- src/sistemaReservaHotel/Hospede.java
+++ src/sistemaReservaHotel/Hospede.java
@@ -1,13 +1,17 @@
 package sistemaReservaHotel;
 
-public class Hospede {
+public class Hospede extends Thread {
 	private String nome;
 	private Quarto quarto;
 	
 	public Hospede(String nome) {
 		this.nome = nome;
 	}
-
+	
+	public void run() {
+      
+    }
+	
 	public void setQuarto(Quarto quarto) {
 		this.quarto = quarto;
 	}
