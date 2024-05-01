f1ddf33c46ae67b90afc54c3572a74ed2eefa459
Bianca Letícia <biancaleticia.moura@gmail.com>
2024-05-01 20:13:32 -0300

    Cria lógica para dividir hospedes caso haja mais que 4 juntos

--- src/sistemaReservaHotel/Recepcionista.java
+++ src/sistemaReservaHotel/Recepcionista.java
@@ -1,4 +1,5 @@
 package sistemaReservaHotel;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -11,17 +12,34 @@ public class Recepcionista extends Thread {
 	}
 
 	public void run() {
-       
+		
     }
 	
 	public synchronized void realizarCheckIn(List<Quarto> quartos, List<Hospede> hospedes) { 
-		for (Quarto quarto: quartos) {
-			if (quarto.disponivel()) {
-				quarto.adicionarHospede(hospedes);
-				return;
+		List<List<Hospede>> sublistaHospede = divideHospedes(hospedes);
+		
+		for (List<Hospede> hospede: sublistaHospede) {
+			for (Quarto quarto: quartos) {
+				if (quarto.disponivel()) {
+					quarto.adicionarHospede(hospede);
+					return;
+				}
 			}
 		}
+		
 		System.out.println("Não foi possível realizar o checkIn");
+	}
+	
+	public List<List<Hospede>> divideHospedes(List<Hospede> hospedes) {
+		List<List<Hospede>> sublistaHospede = new ArrayList<>();
+				
+		for (int comeco = 0; comeco < hospedes.size(); comeco += 4) {
+			int finalDaLista = Math.min(comeco + 4, hospedes.size());
+			List<Hospede> sublista = hospedes.subList(comeco, finalDaLista);
+			sublistaHospede.add(new ArrayList<>(sublista));
+		}
+		
+		return sublistaHospede;
 	}
 	
 	public void adicionarQuartoParaLimpeza() {
