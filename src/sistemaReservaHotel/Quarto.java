ffd8a522ffa8016d2cce378809d8377c2e0ec223
Bianca Letícia <biancaleticia.moura@gmail.com>
2024-04-30 21:11:28 -0300

    Adicionando lista de Hospede ao Quarto

--- src/sistemaReservaHotel/Quarto.java
+++ src/sistemaReservaHotel/Quarto.java
@@ -1,9 +1,11 @@
 package sistemaReservaHotel;
+import java.util.List;
 
 public class Quarto {
 	private int numero;
 	private EstadoQuarto estado = EstadoQuarto.limpo;
 	private int capacidade = 4;
+	private List<Hospede> hospedes;
 	
 	public Quarto(int numero, EstadoQuarto estado) {
 		this.numero = numero;
@@ -17,5 +19,16 @@ public class Quarto {
 	public void setEstado(EstadoQuarto estado) {
 		this.estado = estado;
 	} 
+	
+	public synchronized void adicionarHospede(Hospede hospede) {
+		if (hospedes.size() < capacidade) {
+			hospedes.add(hospede);
+			estado = EstadoQuarto.ocupado;
+		}
+	}
+	
+	public synchronized void removerHospede(Hospede hospede) {
+		hospedes.remove(hospede);
+	}
 	
 }
